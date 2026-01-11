626a0af9553e63dafdc4f8ac4ec57c14c6030a9d
rjcodebytes <136474535+rjcodebytes@users.noreply.github.com>
2026-01-11 14:32:48 +0530

    smooth scrolling done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import CursorDot from "../app/components/cursor";
+import ClientOnly from "../app/components/ClientOnly";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
-        <CursorDot />
+        <ClientOnly>
+          <CursorDot />
+        </ClientOnly>
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect } from "react";
 import Navbar from "../app/components/navbar";
 import Hero from "../app/components/hero";
 import About from "../app/components/about";
@@ -7,7 +10,62 @@ import Footer from "../app/components/footer";
 import Project from "./components/projects";
 import Testimonial from "./components/testimonial";
 
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Lenis from "lenis";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Home() {
+  useEffect(() => {
+    // Initialize Lenis for smooth scrolling
+    const lenis = new Lenis({
+      duration: 1.1,
+      easing: (t) => 1 - Math.pow(1 - t, 3), // easeOutCubic
+      smoothTouch: true,
+      touchMultiplier: 1.2,
+    });
+
+    // Sync Lenis with GSAP's internal ticker
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    // Make ScrollTrigger update on scroll for correct positions
+    const onLenisScroll = () => ScrollTrigger.update();
+    lenis.on("scroll", onLenisScroll);
+
+    // Animate each section on enter with ScrollTrigger
+    const ctx = gsap.context(() => {
+      const sections = gsap.utils.toArray("main > *");
+      sections.forEach((section, i) => {
+        gsap.fromTo(
+          section,
+          { autoAlpha: 0, y: 40 },
+          {
+            autoAlpha: 1,
+            y: 0,
+            duration: 0.8,
+            ease: "power2.out",
+            overwrite: "auto",
+            scrollTrigger: {
+              trigger: section,
+              start: "top 80%",
+              end: "top 30%",
+              toggleActions: "play none none reverse",
+            },
+          }
+        );
+      });
+    });
+
+    return () => {
+      ctx.revert();
+      lenis.off("scroll", onLenisScroll);
+      gsap.ticker.remove((time) => lenis.raf(time * 1000));
+      // Lenis doesn't have a destroy; allow GC to collect
+    };
+  }, []);
   return (
     <>
     
